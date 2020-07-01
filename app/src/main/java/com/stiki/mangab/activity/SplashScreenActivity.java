993eccae5a1dbdc286a293431eecf7e2143a9a91
shellaaf <shellafadwa18@gmail.com>
2020-07-01 17:27:15 +0700

    location

--- app/src/main/java/com/stiki/mangab/activity/SplashScreenActivity.java
+++ app/src/main/java/com/stiki/mangab/activity/SplashScreenActivity.java
@@ -1,19 +1,24 @@
 package com.stiki.mangab.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.location.LocationManagerCompat;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Build;
@@ -26,6 +31,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.stiki.mangab.R;
 import com.stiki.mangab.api.Api;
 import com.stiki.mangab.api.ApiClient;
@@ -35,13 +50,16 @@ import com.stiki.mangab.preference.AppPreference;
 
 import java.net.UnknownHostException;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private Api api;
 
     private Button btnRetry;
-
-    private static String TAG = "checkpermission";
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    PendingResult<LocationSettingsResult> result;
+    final static int REQUEST_LOCATION = 199;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +68,18 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         api = ApiClient.getClient();
 
+        if (isLocationEnabled(this)) {
+            checkPermission();
+        }else{
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this).build();
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private void checkPermission(){
         String[] PERMISSIONS = {
                 Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.CAMERA,
@@ -63,7 +93,11 @@ public class SplashScreenActivity extends AppCompatActivity {
         }else {
             checkStatusLogin();
         }
+    }
 
+    private boolean isLocationEnabled(Context context) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return LocationManagerCompat.isLocationEnabled(locationManager);
     }
 
     private void checkStatusLogin(){
@@ -155,10 +189,10 @@ public class SplashScreenActivity extends AppCompatActivity {
                             isPerpermissionForAllGranted=false;
                         }
                     }
-                    Log.e(TAG, "Permission Granted, Now you can use local drive .");
+                    //Log.e(TAG, "Permission Granted, Now you can use local drive .");
                 } else {
                     isPerpermissionForAllGranted=true;
-                    Log.e(TAG, "Permission Denied, You cannot use local drive .");
+                   // Log.e(TAG, "Permission Denied, You cannot use local drive .");
                 }
                 if(isPerpermissionForAllGranted){
                     checkStatusLogin();
@@ -196,5 +230,89 @@ public class SplashScreenActivity extends AppCompatActivity {
             }
         }
         return imei;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                switch (resultCode) {
+                    case Activity.RESULT_OK: {
+                        // All required changes were successfully made
+                        Toast.makeText(SplashScreenActivity.this, "Location service enabled", Toast.LENGTH_LONG).show();
+                        checkPermission();
+                        break;
+                    }
+                    case Activity.RESULT_CANCELED: {
+                        // The user was asked to change settings, but chose not to
+                        Toast.makeText(SplashScreenActivity.this, "Location not enabled, user cancelled. Please enable..", Toast.LENGTH_LONG).show();
+                        finish();
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+        }
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(30 * 1000);
+        mLocationRequest.setFastestInterval(5 * 1000);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+
+        result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                //final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+                        //...
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    SplashScreenActivity.this,
+                                    REQUEST_LOCATION);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        //...
+                        break;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
